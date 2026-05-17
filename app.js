3c998613966fcdf1393fc0971a227a7cf08063cd
Michael <100490812+DebSol11@users.noreply.github.com>
2026-05-17 19:54:13 +0200

    Displaying the view with EJS

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const app = express();
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
 
-// app.get("/", (req, res) => {
-//   console.log("Here");
-//   res.send("hi");
-// });
+const path = require("node:path");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
